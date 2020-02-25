e2bbe33cd424e745a51e91bac52153db285be924
EC2 Default User <ec2-user@ip-172-31-16-222.us-east-2.compute.internal>
2020-02-25 19:45:06 +0000

    fluency complete

--- app.js
+++ app.js
@@ -1,5 +1,56 @@
 const fetch = require('node-fetch');
 
+function getPost(postId){
+  let post = fetch("https://jsonplaceholder.typicode.com/posts")
+  .then(response => response.json())
+  .then(json => json[postId - 1])
+  return post
+}
+
+function getUsers(){
+  let usernames = fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => response.json())
+  .then(json => json.map(user => user.name))
+  return usernames
+}
+
+function getAddresses(){
+  let addresses = fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => response.json())
+  .then(json => json.map(user => `${user.address.street}, ${user.address.suite}, ${user.address.city}`))
+  return addresses
+}
+
+function shortestPost(){
+  let shortest = fetch("https://jsonplaceholder.typicode.com/posts")
+  .then(response => response.json())
+  .then(json => {
+    let short = ""
+    for(let post of json){
+      if (post.length < short){
+        short = post
+      }
+    }
+    return short
+  })
+  return shortest
+}
+
+function numCompleted(){
+  let completedTasks = fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(json => json.filter(user => user.completed === true).length)
+  return completedTasks
+}
+
+function searchPosts(word){
+  const regex = new RegExp(`ReGeX${word}ReGeX`);
+  let matchingPosts = fetch("https://jsonplaceholder.typicode.com/posts")
+  .then(response => response.json())
+  .then(json => json.filter(post => post.body.match(regex)))
+  return matchingPosts
+}
+
 module.exports = {
   getPost,
   getUsers,
