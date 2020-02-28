0954839a37430b1e2a5b2726e7d1a29b81c1d72c
EC2 Default User <ec2-user@ip-172-31-41-66.ec2.internal>
2020-02-28 00:05:12 +0000

    fetch_fluency_quiz1

--- app.js
+++ app.js
@@ -1,10 +1,59 @@
-const fetch = require('node-fetch');
+//const fetch = require('node-fetch');
 
-module.exports = {
-  // getPost,
-  // getUsers,
-  // getAddresses,
-  // shortestPost,
-  // numCompleted,
-  // searchPosts,
-};
+async function getPost(postId){
+  const url = 'https://jsonplaceholder.typicode.com/posts';
+  const response = await fetch(url);
+  const posts = await response.json();
+  const post = posts.filter(post => post.id === postId);
+  
+  return post[0];
+}
+
+async function getUsers(){
+  const url = 'https://jsonplaceholder.typicode.com/users';
+  const response = await fetch(url);
+  const users = await response.json();
+  
+  return users.map(user => user.name);
+}
+
+async function getAddresses(){
+  const url = 'https://jsonplaceholder.typicode.com/users';
+  const response = await fetch(url);
+  const users = await response.json();
+  
+  return users.map(user => `${user.address.street}, ${user.address.suite}, ${user.address.city}`);
+}
+
+async function shortestPost(){
+  const url = 'https://jsonplaceholder.typicode.com/posts';
+  const response = await fetch(url);
+  const posts = await response.json();
+  
+  return posts.sort(function(a, b){ return b.body.length - a.body.length})[0];
+}
+
+async function numCompleted(){
+  const url = 'https://jsonplaceholder.typicode.com/todos';
+  const response = await fetch(url);
+  const arr = await response.json();
+  
+  return arr.filter(obj => obj.completed === true).length;
+}
+
+// async function searchPosts(searchWord){
+//   const url = 'https://jsonplaceholder.typicode.com/posts';
+//   const response = await fetch(url);
+//   const posts = await response.json();
+  
+//   return posts.filter(post => post.body.includes(searchWord));
+// }
+
+// module.exports = {
+//   getPost,
+//   getUsers,
+//   getAddresses,
+//   shortestPost,
+//   numCompleted,
+//   searchPosts,
+// };
